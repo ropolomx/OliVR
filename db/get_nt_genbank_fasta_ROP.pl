ded7082e2be77441fad13eee8d6eb400f006b638
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2016-09-03 23:45:21 -0600

    Changes to thermo script generator

--- db/get_nt_genbank_fasta_ROP.pl
+++ db/get_nt_genbank_fasta_ROP.pl
@@ -1,6 +1,6 @@
 use LWP::Simple;
 #retrieve unclassified Pestivirus with sequence length limitis
-$query = 'txid12058[Organism:exp]';
+$query = 'txid12058[Organism:exp] NOT txid12110[Organism:exp] AND 100:12000[Sequence Length]';
 
 #assemble the esearch URL
 $base = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
@@ -15,7 +15,7 @@ $key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
 $count = $1 if ($output =~ /<Count>(\d+)<\/Count>/);
 
 #open output file for writing
-open(OUT, ">Picornaviridae.fna") || die "Can't open file!\n";
+open(OUT, ">Picornaviridae_not_FMDV.fna") || die "Can't open file!\n";
 
 #retrieve data in batches of 500
 $retmax = 500;
